1837ce7131f394874b85c21bf10ceb055b4e6b9f
itsakmak <xylopodaros@yahoo.gr>
2025-03-17 13:26:59 +0200

    setting LibCompanion to 0.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'python-dotenv>=1.0.1',
         'boto3>=1.36.7',
         'aws_utils @ git+https://github.com/Envrio-hub/aws_utils.git@1.1.0',
-        'databases_companion @ git+https://github.com/Envrio-hub/LibCompanion.git@main'
+        'databases_companion @ git+https://github.com/Envrio-hub/LibCompanion.git@0.1.0'
     ],
     classifiers=[  
         'Development Status :: 4 - Beta',
